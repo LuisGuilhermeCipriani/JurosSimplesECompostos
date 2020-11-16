f65641b46278c307196c0728ed3f69b98539cb0c
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-16 10:53:56 -0300

    Cria tela de juros compostos

---
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/juros/CompostosActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.luisguilhermecipriani.juros;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompostosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compostos);
+    }
+}
