3f9d93684bc1f1d749f1a08cc62134a6c216716a
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-16 12:34:26 -0300

    Adiciona cálculo de juros simples

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/juros/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/juros/MainActivity.java
@@ -2,13 +2,35 @@ package br.ufjf.dcc196.luisguilhermecipriani.juros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextValorPresente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextValorPresente = findViewById(R.id.editTextValorPresente);
+    }
+
+    public void simplesClick(View view){
+        try{
+            Float valorPresente = Float.parseFloat(editTextValorPresente.getText().toString());
+            Intent simplesIntent = new Intent(getApplicationContext(), SimplesActivity.class);
+            simplesIntent.putExtra("valorPresente", valorPresente);
+            startActivity(simplesIntent);
+        } catch (NumberFormatException ex){
+            editTextValorPresente.selectAll();
+            editTextValorPresente.requestFocus();
+        }
+    }
+
+    public void compostosClick(View view){
+        Intent compostosIntent = new Intent(getApplicationContext(), CompostosActivity.class);
+        startActivity(compostosIntent);
     }
 }
--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/juros/SimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/juros/SimplesActivity.java
@@ -3,12 +3,44 @@ package br.ufjf.dcc196.luisguilhermecipriani.juros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SimplesActivity extends AppCompatActivity {
+    TextView textViewValorPresente;
+    Float valorPresente;
+    EditText editTextTaxa;
+    EditText editTextPeriodos;
+    TextView textViewValorFuturo;
+    Button buttonCalcular;
+    Button buttonRetornar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simples);
+        textViewValorPresente = findViewById(R.id.textViewValorPresente);
+        textViewValorFuturo = findViewById(R.id.textViewValorFuturo);
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getFloat("valorPresente");
+        textViewValorPresente.setText(valorPresente.toString());
+        editTextTaxa = findViewById(R.id.editTextTaxa);
+        editTextTaxa.setText("0.1");
+        editTextPeriodos = findViewById(R.id.editTextPeriodos);
+        editTextPeriodos.setText("10");
+
+    }
+
+    public void calcularClick(View view){
+        Float taxa = Float.parseFloat(editTextTaxa.getText().toString());
+        Integer periodos = Integer.parseInt(editTextPeriodos.getText().toString());
+        Float valorFuturo = valorPresente*(1 + taxa*periodos);
+        textViewValorFuturo.setText(valorFuturo.toString());
+    }
+
+    public void retornarClick(View view){
+        finish();
     }
 }
